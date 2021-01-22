7a81ca279bbb76eb779dbd021b21bbc041224c37
eunicLab <eunicennaji@hotmail.co.uk>
2021-01-22 10:03:17 +0100

    make better

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import './App.css';
-import { IoIosArrowForward } from 'react-icons/io';
-import { IoIosArrowBack } from 'react-icons/io';
-import a0 from './images/autumn_casual_female.jpg';
-import a1 from './images/autumn_casual_male1.jpg';
-import a2 from './images/autumn_cultural_male.png';
-import a3 from './images/winter_cultural_female.jpeg';
-import s0 from './images/summer_casual_female.jpg';
-import s1 from './images/summer_casual_male.jpg';
-import s2 from './images/summer_casual_male1.jpg';
-import s3 from './images/summer_cultural_child.jpeg';
-import s4 from './images/summer_cultural_female.jpg';
-import s5 from './images/summer_cultural_femalee.jpg';
-import s6 from './images/summer_cultural_male.jpg';
-import s7 from './images/summer_formal_female.jpg';
-import s8 from './images/summer_formal_male.jpg';
-import w0 from './images/winter_casual_female.jpg';
-import w1 from './images/winter_casual_male.jpg';
-import w2 from './images/winter_formal_female.jpeg';
-import w3 from './images/winter_formal_male.jpg';
-import w4 from './images/winter_formal_femalee.jpg';
-import w5 from './images/winter_formal_female.jpg';
 
-var winter = [w0, w1, w2, w3, w4, w5];
-var summer = [s0, s1, s2, s3, s4, s5, s6, s7, s8];
+import s0 from './images/summer1.jpg';
+import s1 from './images/summer2.jpeg';
+import s2 from './images/summer3.jpg';
+import s3 from './images/summer4.png';
+import w0 from './images/winter1.jpeg';
+import w1 from './images/winter2.jpg';
+import w2 from './images/winter3.png';
+import w3 from './images/winter4.jpg';
+import a0 from './images/autumn1.jpg';
+import a1 from './images/autumn2.jpg';
+import a2 from './images/autumn3.jpg';
+import a3 from './images/autumn4.jpg';
+
+var winter = [w0, w1, w2, w3];
+var summer = [s0, s1, s2, s3];
 var autumn = [a0, a1, a2, a3];
 var clickCount = 0;
 
@@ -43,6 +35,10 @@ class App extends React.Component {
       country: '',
       displayWidget: 'noDisplay',
       picture: '',
+      carousel0: '',
+      carousel1: '',
+      carousel2: '',
+      carousel3: '',
     };
   }
 
@@ -107,122 +103,134 @@ class App extends React.Component {
         if (this.state.temp <= 10) {
           this.setState({
             picture: winter[clickCount],
+            carousel0: w0,
+            carousel1: w1,
+            carousel2: w2,
+            carousel3: w3,
           });
         }
         if (this.state.temp >= 17) {
           this.setState({
             picture: summer[clickCount],
+            carousel0: s0,
+            carousel1: s1,
+            carousel2: s2,
+            carousel3: s3,
           });
         }
         if (this.state.temp < 17 && this.state.temp > 10) {
           this.setState({
             picture: autumn[clickCount],
+            carousel0: a0,
+            carousel1: a1,
+            carousel2: a2,
+            carousel3: a3,
           });
         }
       });
   };
 
-  handleForwardClick = () => {
-    if (this.state.temp <= 10) {
-      this.setState({ picture: winter[clickCount] });
-      var arrayWeatherPictures = [w0, w1, w2, w3, w4, w5];
-    } else if (this.state.temp >= 17) {
-      this.setState({
-        picture: summer[clickCount],
-      });
-      arrayWeatherPictures = [s0, s1, s2, s3, s4, s5, s6, s7, s8];
-    } else {
-      if (this.state.temp < 17 && this.state.temp > 10) {
-        this.setState({ picture: autumn[clickCount] });
-        arrayWeatherPictures = [a0, a1, a2, a3];
-      }
-    }
-
-    if (clickCount === arrayWeatherPictures.length - 1) {
-      clickCount = 0;
-    } else {
-      clickCount = clickCount + 1;
-    }
-    this.setState({ picture: arrayWeatherPictures[clickCount] });
-  };
-
-  handleBackwardClick = () => {
-    if (this.state.temp <= 10) {
-      this.setState({ picture: winter[clickCount] });
-      var arrayWeatherPictures = [w0, w1, w2, w3, w4, w5];
-    } else if (this.state.temp >= 17) {
-      this.setState({
-        picture: summer[clickCount],
-      });
-      arrayWeatherPictures = [s0, s1, s2, s3, s4, s5, s6, s7, s8];
-    } else {
-      if (this.state.temp < 17 && this.state.temp > 10) {
-        this.setState({ picture: autumn[clickCount] });
-        arrayWeatherPictures = [a0, a1, a2, a3];
-      }
-    }
-    if (clickCount === 0) {
-      clickCount = arrayWeatherPictures.length - 1;
-    } else {
-      clickCount = clickCount - 1;
-    }
-    this.setState({ picture: arrayWeatherPictures[clickCount] });
-  };
-
   render() {
     return (
-      <div className='container'>
-        <form>
-          <input
-            type='text'
-            value={this.state.cityInput}
-            onChange={this.handleChange}
-            id='city'
-            placeholder='Enter Your City Here'
-            required
-          />
+      <div>
+        <div className='container'>
+          <form>
+            <input
+              type='text'
+              value={this.state.cityInput}
+              onChange={this.handleChange}
+              id='city'
+              placeholder='Enter Your City Here'
+              required
+            />
 
-          <button className='btn' onClick={this.handleClick}>
-            Check the weather!
-          </button>
-        </form>
+            <button className='mybtn' onClick={this.handleClick}>
+              Check the weather!
+            </button>
+          </form>
 
-        <div className={this.state.city === '' ? 'noDisplay' : 'card'}>
-          <div className='card-body image-center text-center '>
-            <div className='card-text'>
-              <b>
-                <span>
-                  {this.state.city
-                    ? this.state.city + ', ' + this.state.country + ','
-                    : ''}{' '}
-                </span>
-              </b>
-              <b>
-                {this.state.loading ? (
-                  <h3>loading...</h3>
-                ) : (
-                  this.state.weather + ',  '
-                )}
-              </b>
+          <div className={this.state.city === '' ? 'noDisplay' : 'card'}>
+            <div className='card-body image-center text-center '>
+              <div className='card-text'>
+                <b>
+                  <span>
+                    {this.state.city
+                      ? this.state.city + ', ' + this.state.country + ','
+                      : ''}{' '}
+                  </span>
+                </b>
+                <b>
+                  {this.state.loading ? (
+                    <h3>loading...</h3>
+                  ) : (
+                    this.state.weather + ',  '
+                  )}
+                </b>
 
-              <b>{this.state.city ? this.state.temp : ''}</b>
+                <b>{this.state.city ? this.state.temp : ''}</b>
 
-              <b className={this.state.city === '' ? 'noDisplay' : ''}>
-                <sup>0</sup>C
-              </b>
-              <p>{this.state.city ? this.state.date : ''}</p>
-            </div>
-            <div>
-              <IoIosArrowBack onClick={this.handleBackwardClick} />
-              <img
-                className='pictures'
-                src={this.state.picture}
-                alt='weather'
-              />
-              <IoIosArrowForward onClick={this.handleForwardClick} />
+                <b className={this.state.city === '' ? 'noDisplay' : ''}>
+                  <sup>0</sup>C
+                </b>
+                <p>{this.state.city ? this.state.date : ''}</p>
+              </div>
             </div>
           </div>
-          <div className='card-footer image-center text-center '></div>
+        </div>
+        <div
+          id='myCarousel'
+          class={this.state.city === '' ? 'noDisplay' : 'carousel slide'}
+          data-ride='carousel'>
+          <ol class='carousel-indicators'>
+            <li data-target='#myCarousel' data-slide-to='0' class='active'></li>
+            <li data-target='#myCarousel' data-slide-to='1'></li>
+            <li data-target='#myCarousel' data-slide-to='2'></li>
+            <li data-target='#myCarousel' data-slide-to='3'></li>
+          </ol>
+          <div class='carousel-inner'>
+            <div class='item active text-center'>
+              <img
+                src={this.state.carousel0}
+                alt='weather1'
+                style={{ width: '500px', height: '500px' }}
+              />
+            </div>
+
+            <div class='item'>
+              <img
+                src={this.state.carousel1}
+                alt='weather2'
+                style={{ width: '500px', height: '500px' }}
+              />
+            </div>
+
+            <div class='item'>
+              <img
+                src={this.state.carousel2}
+                alt='weather3'
+                style={{ width: '500px', height: '500px' }}
+              />
+            </div>
+            <div class='item'>
+              <img
+                src={this.state.carousel3}
+                alt='weather4'
+                style={{ width: '500px', height: '500px' }}
+              />
+            </div>
+          </div>
+
+          <a class='left carousel-control' href='#myCarousel' data-slide='prev'>
+            <span class='glyphicon glyphicon-chevron-left'></span>
+            <span class='sr-only'>Previous</span>
+          </a>
+          <a
+            class='right carousel-control'
+            href='#myCarousel'
+            data-slide='next'>
+            <span class='glyphicon glyphicon-chevron-right'></span>
+            <span class='sr-only'>Next</span>
+          </a>
         </div>
       </div>
     );
